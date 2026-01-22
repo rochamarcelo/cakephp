1b3461c9cea7925a10369e1fee88b6782707a0c5
Marcelo Rocha <contato@omarcelo.com.br>
2026-01-22 19:32:13 -0300

    rollback wrong change

--- src/Collection/CollectionInterface.php
+++ src/Collection/CollectionInterface.php
@@ -73,7 +73,7 @@ interface CollectionInterface extends Iterator, JsonSerializable, Countable
      * @param callable|null $callback A callback receiving `($value, $key, $iterator)` that
      *   returns true if the element should be included in the resulting collection.
      *   If left null, a callback that filters out falsey values will be used.
-     * @return self
+     * @return \Cake\Collection\CollectionInterface<TKey, TValue>
      */
     public function filter(?callable $callback = null): CollectionInterface;
 
